392624ae9b4defdb53c825196bd8f039a76d0688
Digambaran <arjunsreekumar.1@gmail.com>
2022-06-15 07:56:48 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Dashboard-graph from './Dashboard-graph'
+export default function App() {
+  return (
+    <div>
+      <Dashboard-graph/>
+    </div>
+  )
+}
---
+++ src/Dashboard-graph.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const Dashboard-graph = () => {
+  return (
+   <p>Hello from Dashboard-graph </p>
+  )
+}
+export default Dashboard-graph
